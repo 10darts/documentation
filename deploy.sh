5011c98791b3417f92828546d0c71dafa1a923b1
Marcos Gabarda <hey@marcosgabarda.com>
2017-10-17 09:57:25 +0200

    deployu script for AWS

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Variables
+# ------------------------------------------------------------------------------
+readonly AWS_S3_BUCKET="docs.10darts.com"
+
+# Buld and upload
+# ------------------------------------------------------------------------------
+rm -rf _build && make html
+aws --profile 10darts s3 sync _build/html/ s3://${AWS_S3_BUCKET}/
