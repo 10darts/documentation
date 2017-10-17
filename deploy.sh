654976ca555ae8a6a3c080e53f3c99017fcfb253
Marcos Gabarda <hey@marcosgabarda.com>
2017-10-17 17:46:14 +0200

    minor

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ readonly AWS_S3_BUCKET="docs.10darts.com"
 # Buld and upload
 # ------------------------------------------------------------------------------
 rm -rf _build && make html
-aws --profile 10darts s3 sync _build/html/ s3://${AWS_S3_BUCKET}/
+aws --profile 10darts s3 sync _build/html/ s3://${AWS_S3_BUCKET}/ --acl public-read
